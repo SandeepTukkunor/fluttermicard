bed6837cb0c4d5a676105319f452d7752e58c4a9
SandeepTukkunor <sandeepbidar1997@gmail.com>
2023-03-08 01:30:45 +0530

    updated main.dart file

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+  runApp(myApp()
   );
 }
+
+class myApp extends StatelessWidget {
+  // const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("Hello World"),
+          backgroundColor: Colors.red,
+        ),
+        body: Container(
+
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+
+        ),
+      ),
+    );
+  }
+}
+
